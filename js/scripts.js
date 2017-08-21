acea90190ff567ca439ed0d7a7a95e7bdef03613
Kristen Kulha and Mitchell Long <kristen@>
2017-08-21 09:15:02 -0700

    create js, html and css files, add place constructor.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+//business logic
+function Place(location,landmarks,timeOfYear,notes) {
+  this.location = location;
+  this.landmarks = landmarks;
+  this.timeOfYear = timeOfYear;
+  this.notes = notes;
+}
+
+//UI
