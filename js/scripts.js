e043fd432c640536ec20e470d0203ed8860524e6
Kristen Kulha and Mitchell Long <kristen@>
2017-08-21 10:52:44 -0700

    Add inputted fields to page after user submits form

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,23 @@ function Place(location,landmarks,timeOfYear,notes) {
 }
 
 //UI
+
+$(document).ready(function(){
+  $("form#new-place").submit(function(event){
+    event.preventDefault();
+    var inputtedLocation = $("input#location").val();
+    var inputtedLandmarks = $("input#landmarks").val();
+    var inputtedTimeOfYear = $("input#timeOfYear").val();
+    var inputtedNotes = $("input#notes").val();
+
+    var newPlace = new Place(inputtedLocation,inputtedLandmarks,inputtedTimeOfYear,inputtedNotes)
+
+    $(".location").append(newPlace.location);
+    debugger;
+    $(".landmarks").text(newPlace.landmarks);
+    $(".timeOfYear").text(newPlace.timeOfYear);
+    $(".notes").text(newPlace.notes);
+  })
+
+
+});
